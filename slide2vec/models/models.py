fe3a6c1e8b8f3798603108d5d2356b32444ad165
clemsgrs <clementnet@outlook.com>
2025-07-21 19:28:20 +0000

    add custom vit model + h0-mini and clean up input_size argument

--- slide2vec/models/models.py
+++ slide2vec/models/models.py
@@ -11,7 +11,8 @@ from timm.data import resolve_data_config
 from timm.data.transforms_factory import create_transform
 
 import slide2vec.distributed as distributed
-import slide2vec.models.vision_transformer as vits
+import slide2vec.models.vision_transformer_dino as vits_dino
+import slide2vec.models.vision_transformer_dinov2 as vits_dinov2
 
 from slide2vec.utils import update_state_dict
 from slide2vec.data.augmentations import make_normalize_transform, MaybeToTensor
@@ -26,19 +27,29 @@ class ModelFactory:
     ):
         if options.level == "tile":
             if options.name == "virchow":
-                model = Virchow(input_size=options.tile_size)
+                model = Virchow(mode=options.mode)
             elif options.name == "virchow2":
-                model = Virchow2(input_size=options.tile_size)
+                model = Virchow2(mode=options.mode)
             elif options.name == "uni":
-                model = UNI(input_size=options.tile_size)
+                model = UNI()
             elif options.name == "uni2":
-                model = UNI2(input_size=options.tile_size)
+                model = UNI2()
             elif options.name == "prov-gigapath":
-                model = ProvGigaPath(input_size=options.tile_size)
+                model = ProvGigaPath()
             elif options.name == "h-optimus-0":
-                model = Hoptimus0(input_size=options.tile_size)
+                model = Hoptimus0()
             elif options.name == "h-optimus-1":
-                model = Hoptimus1(input_size=options.tile_size)
+                model = Hoptimus1()
+            elif options.name == "h-optimus-0-mini" or options.name == "h0-mini":
+                model = Hoptimus0Mini(
+                    mode=options.mode
+                )
+            elif options.name == "rumc-vit-s-50k":
+                model = CustomViT(
+                    arch=options.arch,
+                    pretrained_weights=options.pretrained_weights,
+                    num_register_tokens=0,
+                )
             elif options.name is None and options.arch:
                 model = DINOViT(
                     arch=options.arch,
@@ -47,19 +58,25 @@ class ModelFactory:
                 )
         elif options.level == "region":
             if options.name == "virchow":
-                tile_encoder = Virchow(input_size=options.patch_size)
+                tile_encoder = Virchow()
             elif options.name == "virchow2":
-                tile_encoder = Virchow2(input_size=options.patch_size)
+                tile_encoder = Virchow2()
             elif options.name == "uni":
-                tile_encoder = UNI(input_size=options.patch_size)
+                tile_encoder = UNI()
             elif options.name == "uni2":
-                tile_encoder = UNI2(input_size=options.patch_size)
+                tile_encoder = UNI2()
             elif options.name == "prov-gigapath":
-                tile_encoder = ProvGigaPath(input_size=options.patch_size)
+                tile_encoder = ProvGigaPath()
             elif options.name == "h-optimus-0":
-                tile_encoder = Hoptimus0(input_size=options.patch_size)
+                tile_encoder = Hoptimus0()
             elif options.name == "h-optimus-1":
-                tile_encoder = Hoptimus1(input_size=options.patch_size)
+                tile_encoder = Hoptimus1()
+            elif options.name == "rumc-vit-s-50k":
+                tile_encoder = CustomViT(
+                    arch=options.arch,
+                    pretrained_weights=options.pretrained_weights,
+                    num_register_tokens=0,
+                )
             elif options.name is None and options.arch:
                 tile_encoder = DINOViT(
                     arch=options.arch,
@@ -69,11 +86,11 @@ class ModelFactory:
             model = RegionFeatureExtractor(tile_encoder)
         elif options.level == "slide":
             if options.name == "prov-gigapath":
-                model = ProvGigaPathSlide(input_size=options.tile_size)
+                model = ProvGigaPathSlide()
             elif options.name == "titan":
-                model = TITAN(input_size=options.tile_size)
+                model = TITAN()
             elif options.name == "prism":
-                model = PRISM(input_size=options.tile_size)
+                model = PRISM()
             else:
                 raise ValueError(f"{options.name} doesn't support slide-level encoding")
 
@@ -150,7 +167,7 @@ class DINOViT(FeatureExtractor):
         self.encoder.load_state_dict(state_dict, strict=False)
 
     def build_encoder(self):
-        encoder = vits.__dict__[self.arch](
+        encoder = vits_dino.__dict__[self.arch](
             img_size=self.input_size, patch_size=self.patch_size
         )
         return encoder
@@ -177,9 +194,80 @@ class DINOViT(FeatureExtractor):
         return self.encoder(x)
 
 
+class CustomViT(FeatureExtractor):
+    def __init__(
+        self,
+        arch: str,
+        pretrained_weights: str,
+        num_register_tokens: int = 0,
+        ckpt_key: str = "teacher",
+    ):
+        """_summary_
+
+        Args:
+            arch (str): architecture of the Vision Transformer model
+            pretrained_weights (str): path to the pretrained weights
+            ckpt_key (str, optional): checkpoint key to load the weights. Defaults to "teacher".
+        """
+        self.arch = arch
+        self.pretrained_weights = pretrained_weights
+        self.vit_kwargs = dict(
+            img_size=224,
+            patch_size=14,
+            init_values=1.0e-05,
+            ffn_layer="swiglufused",
+            block_chunks=4,
+            qkv_bias=True,
+            proj_bias=True,
+            ffn_bias=True,
+            num_register_tokens=num_register_tokens,
+            interpolate_offset=0.1,
+            interpolate_antialias=False,
+        )
+        self.ckpt_key = ckpt_key
+        arch2dim = {"vit_large": 1024, "vit_base": 768, "vit_small": 384}
+        self.features_dim = arch2dim[arch]
+        super(CustomViT, self).__init__()
+        self.load_weights()
+
+    def load_weights(self):
+        if distributed.is_main_process():
+            print(f"Loading pretrained weights from: {self.pretrained_weights}")
+        state_dict = torch.load(self.pretrained_weights, map_location="cpu")
+        if self.ckpt_key:
+            state_dict = state_dict[self.ckpt_key]
+        nn.modules.utils.consume_prefix_in_state_dict_if_present(
+            state_dict, prefix="module."
+        )
+        nn.modules.utils.consume_prefix_in_state_dict_if_present(
+            state_dict, prefix="backbone."
+        )
+        state_dict, msg = update_state_dict(self.encoder.state_dict(), state_dict)
+        if distributed.is_main_process():
+            print(msg)
+        self.encoder.load_state_dict(state_dict, strict=False)
+
+    def build_encoder(self):
+        encoder = vits_dinov2.__dict__[self.arch](**self.vit_kwargs)
+        return encoder
+
+    def get_transforms(self):
+        transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Resize(224),
+                transforms.CenterCrop(224),
+                make_normalize_transform(),
+            ]
+        )
+        return transform
+
+    def forward(self, x):
+        return self.encoder(x)
+
+
 class UNI(FeatureExtractor):
-    def __init__(self, input_size: int = 224):
-        self.input_size = input_size
+    def __init__(self):
         self.features_dim = 1024
         super(UNI, self).__init__()
 
@@ -197,8 +285,7 @@ class UNI(FeatureExtractor):
 
 
 class UNI2(FeatureExtractor):
-    def __init__(self, input_size: int = 224):
-        self.input_size = input_size
+    def __init__(self):
         self.features_dim = 1536
         super(UNI2, self).__init__()
 
@@ -228,8 +315,7 @@ class UNI2(FeatureExtractor):
 
 
 class Virchow(FeatureExtractor):
-    def __init__(self, input_size: int = 224, mode: str = "cls"):
-        self.input_size = input_size
+    def __init__(self, mode: str = "cls"):
         self.mode = mode
         self.features_dim = 1280
         if mode == "full":
@@ -261,8 +347,7 @@ class Virchow(FeatureExtractor):
 
 
 class Virchow2(FeatureExtractor):
-    def __init__(self, input_size: int = 224, mode: str = "cls"):
-        self.input_size = input_size
+    def __init__(self, mode: str = "cls"):
         self.mode = mode
         self.features_dim = 1280
         if mode == "full":
@@ -294,8 +379,7 @@ class Virchow2(FeatureExtractor):
 
 
 class ProvGigaPath(FeatureExtractor):
-    def __init__(self, input_size: int = 224):
-        self.input_size = input_size
+    def __init__(self):
         self.features_dim = 1536
         super(ProvGigaPath, self).__init__()
 
@@ -311,8 +395,7 @@ class ProvGigaPath(FeatureExtractor):
 
 
 class Hoptimus0(FeatureExtractor):
-    def __init__(self, input_size: int = 224):
-        self.input_size = input_size
+    def __init__(self):
         self.features_dim = 1536
         super(Hoptimus0, self).__init__()
 
@@ -330,8 +413,7 @@ class Hoptimus0(FeatureExtractor):
 
 
 class Hoptimus1(FeatureExtractor):
-    def __init__(self, input_size: int = 224):
-        self.input_size = input_size
+    def __init__(self):
         self.features_dim = 1536
         super(Hoptimus1, self).__init__()
 
@@ -346,6 +428,38 @@ class Hoptimus1(FeatureExtractor):
 
     def forward(self, x):
         return self.encoder(x)
+
+
+class Hoptimus0Mini(FeatureExtractor):
+    def __init__(self, mode: str = "cls"):
+        self.mode = mode
+        self.features_dim = 768
+        if mode == "full":
+            self.features_dim = 1536
+        super(Hoptimus0Mini, self).__init__()
+
+    def build_encoder(self):
+        encoder = timm.create_model(
+            "hf-hub:bioptimus/H0-mini",
+            pretrained=True,
+            mlp_layer=timm.layers.SwiGLUPacked,
+            act_layer=torch.nn.SiLU,
+        )
+        return encoder
+
+    def forward(self, x):
+        output = self.encoder(x)
+        cls_features = output[:, 0]  # size: 1 x 768
+        patch_token_features = output[
+            :, self.encoder.num_prefix_tokens :
+        ]  # size: 1 x 256 x 768
+        if self.mode == "cls":
+            return cls_features
+        elif self.mode == "full":
+            embedding = torch.cat(
+                [cls_features, patch_token_features.mean(1)], dim=-1
+            )  # size: 1 x 1536
+            return embedding
 
 
 class RegionFeatureExtractor(nn.Module):
@@ -375,8 +489,7 @@ class RegionFeatureExtractor(nn.Module):
 
 
 class SlideFeatureExtractor(nn.Module):
-    def __init__(self, input_size: int = 224):
-        self.input_size = input_size
+    def __init__(self):
         super(SlideFeatureExtractor, self).__init__()
         self.build_encoders()
         self.set_device()
@@ -407,15 +520,14 @@ class SlideFeatureExtractor(nn.Module):
 class ProvGigaPathSlide(SlideFeatureExtractor):
     def __init__(
         self,
-        input_size: int = 224,
     ):
-        super(ProvGigaPathSlide, self).__init__(input_size)
+        super(ProvGigaPathSlide, self).__init__()
         self.features_dim = self.tile_encoder.features_dim
 
     def build_encoders(self):
         import gigapath.slide_encoder as sd
 
-        self.tile_encoder = ProvGigaPath(input_size=self.input_size)
+        self.tile_encoder = ProvGigaPath()
         self.slide_encoder = sd.create_model(
             "hf_hub:prov-gigapath/prov-gigapath",
             "gigapath_slide_enc12l768d",
@@ -430,8 +542,8 @@ class ProvGigaPathSlide(SlideFeatureExtractor):
 
 
 class TITAN(SlideFeatureExtractor):
-    def __init__(self, input_size: int = 512):
-        super(TITAN, self).__init__(input_size)
+    def __init__(self):
+        super(TITAN, self).__init__()
         self.features_dim = 768
 
     def build_encoders(self):
@@ -453,15 +565,15 @@ class TITAN(SlideFeatureExtractor):
 
 
 class PRISM(SlideFeatureExtractor):
-    def __init__(self, input_size: int = 224):
-        super(PRISM, self).__init__(input_size)
+    def __init__(self):
+        super(PRISM, self).__init__()
         self.features_dim = self.tile_encoder.features_dim
 
     def build_encoders(self):
         self.slide_encoder = AutoModel.from_pretrained(
             "paige-ai/PRISM", trust_remote_code=True
         )
-        self.tile_encoder = Virchow(input_size=self.input_size, mode="full")
+        self.tile_encoder = Virchow(mode="full")
 
     def forward_slide(self, tile_features, **kwargs):
         tile_features = tile_features.unsqueeze(0)
