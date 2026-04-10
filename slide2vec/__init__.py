d0a2e3da4c51eacfe243de36bb50bc22f9efb7f6
clemsgrs <clementnet@outlook.com>
2026-04-10 21:28:34 +0000

    Bump version to 4.1.0

--- slide2vec/__init__.py
+++ slide2vec/__init__.py
@@ -2,7 +2,7 @@ from slide2vec.api import EmbeddedSlide, ExecutionOptions, Model, Pipeline, Prep
 from slide2vec.artifacts import HierarchicalEmbeddingArtifact, SlideEmbeddingArtifact, TileEmbeddingArtifact
 
 
-__version__ = "4.0.4"
+__version__ = "4.1.0"
 
 __all__ = [
     "Model",
