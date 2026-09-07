0ba1818e43633f9436a5c5809bdba7ab0ded85a1
clemsgrs <clementnet@outlook.com>
2026-09-07 12:24:06 +0200

    Bump version to 5.9.1

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, str(ROOT))
 project = "slide2vec"
 author = "Clément Grisi"
 copyright = "2026, Clément Grisi"
-release = "5.9.0"
+release = "5.9.1"
 
 extensions = [
     "myst_parser",
--- slide2vec/__init__.py
+++ slide2vec/__init__.py
@@ -27,7 +27,7 @@ from slide2vec.encoders import (
 )
 
 
-__version__ = "5.9.0"
+__version__ = "5.9.1"
 
 __all__ = [
     "Model",
