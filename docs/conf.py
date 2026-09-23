c585455d79b8133503d5a7bde8d1d2d5e6da9e6c
clement grisi <32438483+clemsgrs@users.noreply.github.com>
2026-09-23 11:25:51 +0200

    Bump version to 6.1.0 (#332)

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, str(ROOT))
 project = "slide2vec"
 author = "Clément Grisi"
 copyright = "2026, Clément Grisi"
-release = "6.0.1"
+release = "6.1.0"
 
 extensions = [
     "myst_parser",
